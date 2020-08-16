4b3c9ca20264d6b61b206d1844ba8dcd43722325
sem <mamoshin_sem@mail.ru>
2020-08-16 20:54:20 +0300

    Feature. Render fertilizers name and values.

--- src/molecule/mixtureView/MixtureFertilizers.tsx
+++ src/molecule/mixtureView/MixtureFertilizers.tsx
@@ -2,21 +2,29 @@ import React, {FunctionComponent} from 'react';
 import {Dosage} from "../../models/dosage";
 import {translate} from "../../helpers/translate/translate";
 
+import style from './mixtureView.module.scss'
+
+
 interface MixtureFertilizersProps {
     dosages: Dosage[]
 }
 
 const MixtureFertilizers: FunctionComponent<MixtureFertilizersProps> = ({dosages}) => {
+
+    const renderDosages = () => {
+        return dosages.map(dosage => {
+            return (
+                <div className={style.fertilizerLine} key={dosage.fertilizer.id}>
+                    <span className={style.name}>{dosage.fertilizer.name}</span>
+                    <span className={style.value}>{dosage.value} {translate('gramLiter')}</span>
+                </div>
+            )
+        })
+    }
+
     return (
-        <div>
-            {dosages.map(dosage => {
-                return (
-                    <div key={dosage.fertilizer.id}>
-                        {dosage.fertilizer.name}: {dosage.value} {translate('gramLiter')}
-                    </div>
-                )
-            })
-            }
+        <div className={style.fertilizerBox}>
+            {renderDosages()}
         </div>
     );
 };
