71495e82e6e9ea4547003c4d56f2eba80faaebd8
sem <mamoshin_sem@mail.ru>
2020-08-24 12:29:40 +0300

    Refactor. mixtures mock

--- src/mocks/mixturesMock.ts
+++ src/mocks/mixturesMock.ts
@@ -1,4 +1,5 @@
 import {Mixture} from "../models/mixture";
+import { FertilizerIngredient } from "models/fertilizerIngredient";
 
 export const mixturesMock: Mixture[] = [
     new Mixture(
@@ -9,16 +10,8 @@ export const mixturesMock: Mixture[] = [
                     id: '806d985b-3319-4a27-b0d3-89aafb48d09e',
                     name: 'Удобрение 2',
                     ingredients: [
-                        {
-                            id: 'c81915c0-e114-4c85-bcc5-b671b5027c7d',
-                            chemicalComplexId: '2P',
-                            valuePercent: 10
-                        },
-                        {
-                            id: 'd245d35c-4c20-49eb-a3c9-e16a34298a90',
-                            chemicalComplexId: '3K',
-                            valuePercent: 3.2
-                        }
+                        new FertilizerIngredient('2P', 10, 'c81915c0-e114-4c85-bcc5-b671b5027c7d'),
+                        new FertilizerIngredient('3K', 3.2, 'd245d35c-4c20-49eb-a3c9-e16a34298a90')
                     ]
                 },
                 valueGram: 11
@@ -28,11 +21,7 @@ export const mixturesMock: Mixture[] = [
                     id: '9f141020-190f-47a5-a320-22a9404cc439',
                     name: 'Удобрение 1',
                     ingredients: [
-                        {
-                            id: '009d3587-2fbb-4bde-a4a5-abc5054130a8',
-                            chemicalComplexId: '1N',
-                            valuePercent: 33
-                        }
+                        new FertilizerIngredient('1N', 33, '009d3587-2fbb-4bde-a4a5-abc5054130a8')
                     ]
                 },
                 valueGram: 22
@@ -42,21 +31,9 @@ export const mixturesMock: Mixture[] = [
                     id: 'aeb89623-3085-4e82-8927-a8c48ef93f31',
                     name: 'Удобрение 3',
                     ingredients: [
-                        {
-                            id: '2b346d3a-9144-44b5-876d-f57d71835b83',
-                            chemicalComplexId: '5M',
-                            valuePercent: 1.2
-                        },
-                        {
-                            id: '1991ccdd-a335-4bc8-9b71-1c3c286ea132',
-                            chemicalComplexId: '4C',
-                            valuePercent: 0.5
-                        },
-                        {
-                            id: '62475a3f-3f26-41e1-ace3-741c36c089f8',
-                            chemicalComplexId: '1N',
-                            valuePercent: 0.3
-                        }
+                        new FertilizerIngredient('5M', 1.2, '2b346d3a-9144-44b5-876d-f57d71835b83'),
+                        new FertilizerIngredient('4C', 0.5, '1991ccdd-a335-4bc8-9b71-1c3c286ea132'),
+                        new FertilizerIngredient('1N', 0.3, '62475a3f-3f26-41e1-ace3-741c36c089f8')
                     ]
                 },
                 valueGram: 22
@@ -72,16 +49,8 @@ export const mixturesMock: Mixture[] = [
                     id: '806d985b-3319-4a27-b0d3-89aafb48d09e',
                     name: 'Удобрение 2',
                     ingredients: [
-                        {
-                            id: 'c81915c0-e114-4c85-bcc5-b671b5027c7d',
-                            chemicalComplexId: '2P',
-                            valuePercent: 10
-                        },
-                        {
-                            id: 'd245d35c-4c20-49eb-a3c9-e16a34298a90',
-                            chemicalComplexId: '3K',
-                            valuePercent: 3.2
-                        }
+                        new FertilizerIngredient('2P', 10, 'c81915c0-e114-4c85-bcc5-b671b5027c7d'),
+                        new FertilizerIngredient('3K', 3.2, 'd245d35c-4c20-49eb-a3c9-e16a34298a90'),
                     ]
                 },
                 valueGram: 11
@@ -91,11 +60,7 @@ export const mixturesMock: Mixture[] = [
                     id: '9f141020-190f-47a5-a320-22a9404cc439',
                     name: 'Удобрение 1',
                     ingredients: [
-                        {
-                            id: '009d3587-2fbb-4bde-a4a5-abc5054130a8',
-                            chemicalComplexId: '1N',
-                            valuePercent: 33
-                        }
+                        new FertilizerIngredient('1N' , 33, '009d3587-2fbb-4bde-a4a5-abc5054130a8')
                     ]
                 },
                 valueGram: 2.2
@@ -105,21 +70,9 @@ export const mixturesMock: Mixture[] = [
                     id: 'aeb89623-3085-4e82-8927-a8c48ef93f31',
                     name: 'Удобрение 3',
                     ingredients: [
-                        {
-                            id: '2b346d3a-9144-44b5-876d-f57d71835b83',
-                            chemicalComplexId: '5M',
-                            valuePercent: 1.2
-                        },
-                        {
-                            id: '1991ccdd-a335-4bc8-9b71-1c3c286ea132',
-                            chemicalComplexId: '4C',
-                            valuePercent: 0.5
-                        },
-                        {
-                            id: '62475a3f-3f26-41e1-ace3-741c36c089f8',
-                            chemicalComplexId: '1N',
-                            valuePercent: 0.3
-                        }
+                        new FertilizerIngredient('5M' , 1.2, '2b346d3a-9144-44b5-876d-f57d71835b83'),
+                        new FertilizerIngredient('4C' , 0.5, '1991ccdd-a335-4bc8-9b71-1c3c286ea132'),
+                        new FertilizerIngredient('1N' , 0.3, '62475a3f-3f26-41e1-ace3-741c36c089f8')
                     ]
                 },
                 valueGram: 31.23
