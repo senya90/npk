499467d40d5fa8212c8dd6a20f2f5922315d55ed
sem <mamoshin_sem@mail.ru>
2020-08-27 20:40:26 +0300

    Test. Create hand calculations. TODO: calculate

--- src/models/mixture/mixture.test.ts
+++ src/models/mixture/mixture.test.ts
@@ -18,11 +18,17 @@ describe('Mixture', () => {
         it('correct work', () => {
             const mixture = mixturesMock[2]
             const result = mixture.toChemicals()
-            console.log(result);
-            
 
             expect(result).toEqual([
-                new ChemicalUnitValue(chemicalUnitsMock.Mg, 0)
+                new ChemicalUnitValue(chemicalUnitsMock.Mg, 0),
+                new ChemicalUnitValue(chemicalUnitsMock.S, 0),
+                new ChemicalUnitValue(chemicalUnitsMock.O, 0),
+                new ChemicalUnitValue(chemicalUnitsMock.H, 0),
+                new ChemicalUnitValue(chemicalUnitsMock.P, 0),
+                new ChemicalUnitValue(chemicalUnitsMock.K, 0),
+                new ChemicalUnitValue(chemicalUnitsMock.Ca, 0),
+                new ChemicalUnitValue(chemicalUnitsMock.N, 0),
+                new ChemicalUnitValue(chemicalUnitsMock.Fe, 0),
             ])
         })
 
