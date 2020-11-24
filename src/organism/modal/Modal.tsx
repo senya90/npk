81b8d2a9648e28ca39432802f41ea4a466c9cbbe
sem <mamoshin_sem@mail.ru>
2020-11-24 18:11:41 +0300

    Close modal method

--- src/organism/modal/Modal.tsx
+++ src/organism/modal/Modal.tsx
@@ -1,4 +1,4 @@
-import React, {FC} from 'react';
+import React, {FC, useCallback} from 'react';
 import ReactDOM from 'react-dom'
 import { useCreateModalRoot } from './useCreateModalRoot';
 import { createModalElement } from './createModalElement';
@@ -7,22 +7,36 @@ import { Icon } from 'atom/icon/Icon';
 import {ICON_TYPE} from "../../atom/icon/IconTypes";
 
 interface ModalProps {
-
+    title: string
+    onClose?: (id: string) => void
 }
 
-const Modal: FC<ModalProps> = ({children}) => {
+const Modal: FC<ModalProps> = (props) => {
+    const {onClose} = props
     const modalDom = useCreateModalRoot('modal-root')
+    const {element, id} = createModalElement(modalDom)
 
-    if (!modalDom) {
-        return null
+    const closeModal = useCallback(() => {
+        if (onClose) {
+            onClose(id)
+        }
+    }, [id, onClose])
+
+    const closeModalByOut = (e: React.MouseEvent) => {
+        if (e.target === e.currentTarget) {
+            closeModal()
+        }
     }
 
     const getModal = () => {
         return (
-            <div className={style.modal}>
-                <div className={style.modalWindow}>
+            <div className={style.modal} onClick={closeModalByOut}>
+                <div className={style.modalWindow} >
                     <div className={style.header}>
-                        <div className={style.closeModal}>
+                        {props.title &&
+                            <div className={style.modalTitle}>{props.title}</div>
+                        }
+                        <div className={style.closeModal} onClick={closeModal}>
                             <Icon
                                 type={ICON_TYPE.Cross}
                                 size={20}
@@ -30,14 +44,12 @@ const Modal: FC<ModalProps> = ({children}) => {
                         </div>
                     </div>
                     <div className={style.modalContent}>
-                        {children}
+                        {props.children}
                     </div>
                 </div>
             </div>
         )
     }
-
-    const {element} = createModalElement(modalDom)
 
     return (
         ReactDOM.createPortal(
