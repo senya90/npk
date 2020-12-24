7a8bde692309c7eb7ce07e8b1ec3a1330c8787d0
sem <mamoshin_sem@mail.ru>
2020-12-24 10:14:01 +0300

    Feature. Add fade for zero value

--- src/molecule/chemicalComparisonView/ChemicalComparisonView.tsx
+++ src/molecule/chemicalComparisonView/ChemicalComparisonView.tsx
@@ -3,6 +3,8 @@ import {TableRaw} from "../../organism/table/tableRow/TableRaw";
 import {TableCell} from "../../organism/table/tableCell/TableCell";
 import {ChemicalUnit} from "../../models/chemicalUnit";
 import { Utils } from 'helpers/utils';
+import cn from 'classnames'
+import style from './chemicalComparisonView.module.scss'
 
 interface ChemicalComparisonViewProps {
     chemical: ChemicalUnit
@@ -13,12 +15,20 @@ interface ChemicalComparisonViewProps {
 
 const ChemicalComparisonView: FunctionComponent<ChemicalComparisonViewProps> = (props) => {
 
+    const mixed = Utils.round(props.mixed, 1)
+    const vegetation = Utils.round(props.vegetation, 1)
+    const bloom = Utils.round(props.bloom, 1)
+
+    const getFadeStyle = (fieldValue: number): string => {
+        return cn({[style.fade]: fieldValue === 0})
+    }
+
     return (
             <TableRaw>
                 <TableCell>{props.chemical.name}</TableCell>
-                <TableCell>{Utils.round(props.mixed, 1)}</TableCell>
-                <TableCell>{Utils.round(props.vegetation,1)}</TableCell>
-                <TableCell>{Utils.round(props.bloom,1)}</TableCell>
+                <TableCell className={getFadeStyle(mixed)}>{mixed}</TableCell>
+                <TableCell className={getFadeStyle(vegetation)}>{vegetation}</TableCell>
+                <TableCell className={getFadeStyle(bloom)}>{bloom}</TableCell>
             </TableRaw>
     );
 };
