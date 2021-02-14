f284892d7ab0b0ebd7ec3d6261c7f84459ee5b04
senya <mamoshin_sem@mail.ru>
2021-02-14 12:07:34 +0300

    Feature. log out button

--- src/organism/header/Header.tsx
+++ src/organism/header/Header.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import cn from 'classnames'
 import {NavLink} from 'react-router-dom';
 
 import {translate} from "helpers/translate/translate";
 import {ROUTES} from 'core/routes/routes';
 
 import style from './header.module.scss'
+import { useSelector } from 'react-redux';
+import {TokenHelper} from "../../helpers/tokens";
 
 const Header = () => {
+    const tokens = useSelector((state: any) => state.user.tokens)
+    const isAuth = TokenHelper.isActive(tokens.accessToken)
+
+    const logout = () => {
+        
+    }
+
     return (
         <div className={style.header}>
             <div className={style.headerContainer}>
@@ -27,13 +37,22 @@ const Header = () => {
                     >
                         {translate('calculatorPage')}
                     </NavLink>
-                    <NavLink
-                        to={ROUTES.LOGIN}
-                        className={style.link}
-                        activeClassName={style.activeLink}
-                    >
-                        {translate('login')}
-                    </NavLink>
+                    {isAuth ?
+                        <div
+                            className={cn(style.link, style.logout)}
+                            onClick={logout}
+                        >
+                            {translate('logout')}
+                        </div>
+                        :
+                        <NavLink
+                            to={ROUTES.LOGIN}
+                            className={style.link}
+                            activeClassName={style.activeLink}
+                        >
+                            {translate('login')}
+                        </NavLink>
+                    }
                 </nav>
             </div>
         </div>
