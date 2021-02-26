eef77aa37d79cc0c7acbd3a275a6e77ef66de9c0
senya <mamoshin_sem@mail.ru>
2021-02-26 18:47:49 +0300

    Feature,. Autp update tokens on request

--- src/core/api/index.ts
+++ src/core/api/index.ts
@@ -5,10 +5,14 @@ import {INotificationService} from "../services/notificationService/Notification
 import {NotificationHelper} from "../../helpers/notifications/notificationHelper";
 import {LocalStorageProvider} from "../services/localStorageProvider/LocalStorageProvider";
 import {ILocalStorageProvider} from "../services/localStorageProvider/LocalStorageProviderTypes";
+import {UserService} from "../services/userService/UserService";
+import {IUserService} from "../services/userService/UserServiceTypes";
+import {TokenHelper} from "../../helpers/tokens";
 
 interface IAPI {
     notificationService: INotificationService
     localStorageService: ILocalStorageProvider
+    userService: IUserService
 
     get: (apiURL: string, apiParams?: any, headers?: any) => Promise<any>
     getAuthorized: (apiURL: string, apiParams?: any, headers?: any) => Promise<any>
@@ -22,6 +26,7 @@ interface IAPI {
 export const API: IAPI = {
     notificationService: new NotificationService(store.dispatch),
     localStorageService: new LocalStorageProvider(),
+    userService: new UserService(store.dispatch, new LocalStorageProvider()),
     get: async function (apiURL: string, apiParams?: any, headers?: any): Promise<any> {
         return request.get(apiURL, apiParams, headers)
             .catch(err => {
@@ -39,12 +44,26 @@ export const API: IAPI = {
         const authHeader = {'Authorization': `Bearer ${accessToken}`}
         return this.get(apiURL, apiParams, {...headers, ...authHeader})
     },
-    postAuthorized: function(apiURL: string, apiParams?: any, headers?: any) {
+    postAuthorized: async function(apiURL: string, apiParams?: any, headers?: any) {
         const tokens = this.localStorageService.getTokens()
         let accessToken;
         if (tokens) {
             accessToken = tokens.accessToken
+            const isActive = TokenHelper.isActive(tokens.accessToken)
+
+            if (!isActive) {
+                try {
+                    const updatedTokens = await TokenHelper.updateTokens(tokens.refreshToken)
+                    if (updatedTokens) {
+                        await this.userService.updateTokens(updatedTokens);
+                        accessToken = updatedTokens.accessToken
+                    }
+                } catch (err) {
+                    console.log('Error auto update tokens', err)
+                }
+            }
         }
+
         const authHeader = {'Authorization': `Bearer ${accessToken}`}
         return this.post(apiURL, apiParams, {...headers, ...authHeader})
     },
