d2c7f355361197bef12c68cb948c6291128dd2a8
senya <mamoshin_sem@mail.ru>
2021-03-12 16:07:57 +0300

    Feature. Popover for agriculture delete

--- src/organism/agriclulturesView/agricultureItem/AgricultureItem.tsx
+++ src/organism/agriclulturesView/agricultureItem/AgricultureItem.tsx
@@ -1,4 +1,4 @@
-import React, {FunctionComponent, useContext} from 'react';
+import React, {FunctionComponent, useContext, useState} from 'react';
 import {Agriculture} from "../../../models/agriculture";
 import {CalculatorContext} from "../../../helpers/contexts/CalculatorContext";
 import cn from 'classnames'
@@ -6,6 +6,11 @@ import cn from 'classnames'
 import style from './agricultureItem.module.scss'
 import { Icon } from 'atom/icon/Icon';
 import {ICON_TYPE} from "../../../atom/icon/IconTypes";
+import {translate} from "../../../helpers/translate/translate";
+import stylePopover from "../../../atom/popover/popover.module.scss";
+import {Button} from "../../../atom/button/Button";
+import {BUTTON_TYPE} from "../../../atom/button/ButtonTypes";
+import {Popover} from "../../../atom/popover/Popover";
 
 
 interface AgricultureItemProps {
@@ -17,6 +22,7 @@ interface AgricultureItemProps {
 
 const AgricultureItem: FunctionComponent<AgricultureItemProps> = ({agriculture, isActive, onEdit, onDelete}) => {
     const {onAgricultureSelect} = useContext(CalculatorContext)
+    const [isShowTooltip, setIsShowTooltip] = useState<boolean>(false)
 
     const selectAgriculture = () => {
         onAgricultureSelect(agriculture)
@@ -28,6 +34,20 @@ const AgricultureItem: FunctionComponent<AgricultureItemProps> = ({agriculture, 
 
     const deleteAgriculture = () => {
         onDelete(agriculture)
+    }
+
+    const onVisibleChange = (visible: boolean) => {
+        setIsShowTooltip(visible)
+    }
+
+    const closeTooltip = (e: React.MouseEvent) => {
+        e.stopPropagation()
+        setIsShowTooltip(false)
+    }
+
+    const showTooltip = (e: React.MouseEvent) => {
+        e.stopPropagation()
+        setIsShowTooltip(true)
     }
 
     return (
@@ -42,12 +62,37 @@ const AgricultureItem: FunctionComponent<AgricultureItemProps> = ({agriculture, 
                     className={style.controlsIcon}
                     size={18}
                 />
-                <Icon
-                    type={ICON_TYPE.Delete}
-                    className={style.controlsIcon}
-                    size={18}
-                    onClick={deleteAgriculture}
-                />
+                <Popover
+                    visible={isShowTooltip}
+                    title={`${translate('deleteAgriculture')}?`}
+                    onVisibleChange={onVisibleChange}
+                    content={
+                        <div className={stylePopover.modalButtonsBox}>
+                            <Button
+                                className={stylePopover.modalButton}
+                                onClick={closeTooltip}
+                            >
+                                {translate('cancel')}
+                            </Button>
+                            <Button
+                                className={stylePopover.modalButton}
+                                danger
+                                type={BUTTON_TYPE.PRIMARY}
+                                onClick={deleteAgriculture}
+                            >
+                                {translate('delete')}
+                            </Button>
+                        </div>
+                    }
+                >
+                    <Icon
+                        type={ICON_TYPE.Delete}
+                        className={style.controlsIcon}
+                        size={18}
+                        onClick={showTooltip}
+                    />
+                </Popover>
+
             </div>
 
         </div>
